2c667d20b6771bc199e09308877833c48cc03f79
fikri abdulhakim <80445849+fikriabdulhakim@users.noreply.github.com>
2026-05-17 20:42:15 +0200

    Update settings.py

--- cru_clone/settings.py
+++ cru_clone/settings.py
@@ -3,7 +3,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'muslim-ruhr.vercel.app',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
